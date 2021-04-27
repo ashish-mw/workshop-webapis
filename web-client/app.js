5c43763142c78120413b90d48017550b036ed303
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2021-04-27 15:41:30 +0530

    refactor web client

--- web-client/app.js
+++ web-client/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3333';
+const API_URL =  localStorage.getItem('url') || 'http://localhost:3333';
 
 const app = new Vue({
   el: '#app',
@@ -12,34 +12,41 @@ const app = new Vue({
     this.fetchData();
   },
   methods: {
-    fetchData: function () {
+    fetchDoctors: function () {
       let _self = this;
-      _self.isServiceUsersLoading = true;
       _self.isDoctorsLoading = true;
       fetch(`${API_URL}/doctors`)
-        .then(res => res.json())
-        .then(json => {
-          _self.isDoctorsLoading = false;
-          _self.doctors = json.data
-        })
-        .catch(e => {
-          console.error(e);
-          _self.isDoctorsLoading = false;
-        });
+      .then(res => res.json())
+      .then(json => {
+        _self.isDoctorsLoading = false;
+        _self.doctors = json.data
+      })
+      .catch(e => {
+        console.error(e);
+        _self.isDoctorsLoading = false;
+      });
+    },
+    fetchServiceUsers: function () {
+      let _self = this;
+      _self.isServiceUsersLoading = true;
       fetch(`${API_URL}/service-users`)
-        .then(res => res.json())
-        .then(json => {
-          _self.isServiceUsersLoading = false;
-          _self.serviceUsers = json.data.map(d => {
-            let newD = { ...d }
-            newD.admittedDateLocale = new Date(d.admittedDate).toLocaleString();
-            return newD;
-          })
+      .then(res => res.json())
+      .then(json => {
+        _self.isServiceUsersLoading = false;
+        _self.serviceUsers = json.data.map(d => {
+          let newD = { ...d }
+          newD.admittedDateLocale = new Date(d.admittedDate).toLocaleString();
+          return newD;
         })
-        .catch(e => {
-          console.error(e);
-          _self.isServiceUsersLoading = false;
-        });
+      })
+      .catch(e => {
+        console.error(e);
+        _self.isServiceUsersLoading = false;
+      });
+    },
+    fetchData: function () {
+      this.fetchDoctors();
+      this.fetchServiceUsers();
     }
   }
 })
